ac0251233863649398ed2f54c6848a73035c9069
MSmart <matt.smart14@gmail.com>
2024-03-27 11:38:30 -0500

    set up back end portion

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,66 @@
+import {gql} from '@apollo/client';
+
+
+export const ADD_USER = gql `
+mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        email
+        username
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql `
+mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql `
+mutation SaveBook($bookdata: BookInput!) {
+    saveBook(bookdata: $bookdata) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql `
+mutation RemoveBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
